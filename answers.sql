4a0612ddbff8e6ae26b39d69a3ba3ee89df5ce37
treyrey-212 <tracyreson@gmail.com>
2025-03-21 14:31:27 +0300

    Add files via upload
    database assignment week 1

---
+++ answers.sql
@@ -0,0 +1,7 @@
+-- Question 1: Create a new database called salesDB.
+-- This query creates a new database called 'salesDB'.
+CREATE DATABASE salesDB;
+
+-- Question 2: Drop (delete) the database called demo.
+-- This query will permanently remove the 'demo' database and all its data.
+DROP DATABASE demo;
